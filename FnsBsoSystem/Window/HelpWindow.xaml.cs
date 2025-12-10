47e7f75360ad0f792ab7370c8683b41275ab7a5b
Haruki <Haruki@DESKTOP-3JGMB3L>
2025-12-10 17:35:35 +0300

    Add project files.

---
+++ FnsBsoSystem/Window/HelpWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MdXaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FnsBsoSystem.Window
+{
+    /// <summary>
+    /// Interaction logic for HelpWindow.xaml
+    /// </summary>
+    public partial class HelpWindow
+    {
+        public HelpWindow()
+        {
+            InitializeComponent();
+            Markdown engine = new Markdown();
+            string markdownTxt = File.ReadAllText("README.md");
+            FlowDocument document = engine.Transform(markdownTxt);
+
+            MyDocumentDisplay.Document = document;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Перетаскивать окно мышкой
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Позволяет перетаскивать окно
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
